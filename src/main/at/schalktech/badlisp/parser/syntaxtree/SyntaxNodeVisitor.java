56bff87ee1b0876001c6d015fb4c50bcf15cd514
Moritz Schalk <schalk.moritz@tutanota.de>
2022-01-31 12:25:26 +0100

    Added syntax node visitor.

--- src/main/at/schalktech/badlisp/parser/syntaxtree/SyntaxNodeVisitor.java
+++ src/main/at/schalktech/badlisp/parser/syntaxtree/SyntaxNodeVisitor.java
@@ -2,4 +2,6 @@ package at.schalktech.badlisp.parser.syntaxtree;
 
 public interface SyntaxNodeVisitor {
     void visitListNode(ListNode listNode);
+    void visitSymbolNode(SymbolNode symbolNode);
+    void visitIntegerNode(IntegerNode integerNode);
 }
