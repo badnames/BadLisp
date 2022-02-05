292c921daa5cc7e89f4ef2221e44c34a143e0f70
Moritz Schalk <schalk.moritz@tutanota.de>
2022-02-05 14:09:48 +0100

    Added SyntaxErrorException.

---
+++ src/main/at/schalktech/badlisp/parser/syntaxtree/SyntaxErrorException.java
@@ -0,0 +1,19 @@
+package at.schalktech.badlisp.parser.syntaxtree;
+
+public class SyntaxErrorException extends Exception {
+
+    private int position;
+    private String message;
+
+    public SyntaxErrorException(int position, String message) {
+        super();
+
+        this.position = position;
+        this.message = message;
+    }
+
+    @Override
+    public String getMessage() {
+        return "Syntax error at or near position " + position + ": " + message;
+    }
+}
