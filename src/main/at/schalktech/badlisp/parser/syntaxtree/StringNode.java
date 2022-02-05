eca9d91a3af8e03729c41ce178156ea782ea88f8
Moritz Schalk <schalk.moritz@tutanota.de>
2022-02-05 14:10:43 +0100

    Added StringNode.

---
+++ src/main/at/schalktech/badlisp/parser/syntaxtree/StringNode.java
@@ -0,0 +1,34 @@
+package at.schalktech.badlisp.parser.syntaxtree;
+
+public class StringNode implements SyntaxNode{
+
+    private String string;
+
+    public StringNode(String string) {
+        this.string = string;
+    }
+
+    @Override
+    public SyntaxNode getChild(int num) {
+        return null;
+    }
+
+    @Override
+    public int getChildCount() {
+        return 0;
+    }
+
+    @Override
+    public void addChild(SyntaxNode node) {
+
+    }
+
+    @Override
+    public void accept(SyntaxNodeVisitor visitor) {
+        visitor.visitStringNode(this);
+    }
+
+    public String getString() {
+        return string;
+    }
+}
