8ba88941a0b11d59b9295795d89448c546358f54
Moritz Schalk <schalk.moritz@tutanota.de>
2022-02-05 14:11:01 +0100

    Added missing methods to SyntaxNodeVisitor.

--- src/main/at/schalktech/badlisp/parser/syntaxtree/SyntaxNodeVisitor.java
+++ src/main/at/schalktech/badlisp/parser/syntaxtree/SyntaxNodeVisitor.java
@@ -4,4 +4,6 @@ public interface SyntaxNodeVisitor {
     void visitListNode(ListNode listNode);
     void visitSymbolNode(SymbolNode symbolNode);
     void visitIntegerNode(IntegerNode integerNode);
+    void visitRationalNode(RationalNode rationalNode);
+    void visitStringNode(StringNode stringNode);
 }
